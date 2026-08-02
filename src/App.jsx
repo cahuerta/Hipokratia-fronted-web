bb0434502d65b4e5e0c13d9a43452138a9459639
cahuerta <cahuerta@gmail.com>
2026-08-01 20:44:38 -0400

    Replace routes for MiSalud and others with new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
 import Home from './pages/Home.jsx'
-import MiSalud from './pages/MiSalud.jsx'
-import FichaClinica from './pages/FichaClinica.jsx'
-import EvidenciaMed from './pages/EvidenciaMed.jsx'
-import Docencia from './pages/Docencia.jsx'
+import Portal from './pages/Portal.jsx'
+import Gestion from './pages/Gestion.jsx'
+import Dossier from './pages/Dossier.jsx'
+import Catedra from './pages/Catedra.jsx'
 import Contacto from './pages/Contacto.jsx'
 
 export default function App() {
@@ -12,10 +12,10 @@ export default function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/misalud" element={<MiSalud />} />
-        <Route path="/ficha-clinica" element={<FichaClinica />} />
-        <Route path="/evidenciamed" element={<EvidenciaMed />} />
-        <Route path="/docencia" element={<Docencia />} />
+        <Route path="/portal" element={<Portal />} />
+        <Route path="/gestion" element={<Gestion />} />
+        <Route path="/dossier" element={<Dossier />} />
+        <Route path="/catedra" element={<Catedra />} />
         <Route path="/contacto" element={<Contacto />} />
       </Route>
     </Routes>
