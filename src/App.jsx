3d3fe9b75bfc7ed682c39f0a6032120819b26d58
cahuerta <cahuerta@gmail.com>
2026-08-18 11:40:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
 import Home from './pages/Home.jsx'
+import Pacientes from './pages/Pacientes.jsx'
+import Profesionales from './pages/Profesionales.jsx'
 import Portal from './pages/Portal.jsx'
 import Gestion from './pages/Gestion.jsx'
 import Dossier from './pages/Dossier.jsx'
@@ -13,6 +15,8 @@ export default function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/pacientes" element={<Pacientes />} />
+        <Route path="/profesionales" element={<Profesionales />} />
         <Route path="/portal" element={<Portal />} />
         <Route path="/gestion" element={<Gestion />} />
         <Route path="/dossier" element={<Dossier />} />
